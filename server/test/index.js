35045610a75b161eb7be80696ac6b194c653a457
Tunde Yakub <tundenasri@gmail.com>
2019-06-24 16:52:12 +0100

    setup travis

--- server/test/index.js
+++
@@ -1,9 +0,0 @@
-import chai from 'chai';
-
-const { expect } = chai;
-describe('it should test mocha', () => {
-  it('test addition', (done) => {
-    expect(2 + 2).to.eql(4);
-    done();
-  });
-});
