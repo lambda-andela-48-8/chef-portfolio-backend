17502e46a9985a1263e0ffcb940b67796cad100c
Tunde Yakub <tundenasri@gmail.com>
2019-06-25 19:06:20 +0100

    write test for login route

--- server/test/auth.spec.js
+++ server/test/auth.spec.js
@@ -37,3 +37,51 @@ describe('POST /api/auth/signup', () => {
       });
   });
 });
+
+describe('POST /api/auth/login', () => {
+  it('should return error when email or password is not passed along request', (done) => {
+    chai.request(server)
+      .post('/api/auth/login')
+      .send({})
+      .end((err, res) => {
+        expect(res.status).to.eql(422);
+        expect(res.body.status).to.eql('failed');
+        expect(res.body.error).to.eql('email and password required to login');
+        done();
+      });
+  });
+  it('should return error when email is incorrect', (done) => {
+    chai.request(server)
+      .post('/api/auth/login')
+      .send({ email: 'noexist@mail.com', password: 'secret' })
+      .end((err, res) => {
+        expect(res.status).to.eql(404);
+        expect(res.body.status).to.eql('failed');
+        expect(res.body.error).to.eql('email or password is incorrect');
+        done();
+      });
+  });
+  it('should return error when password is incorrect', (done) => {
+    chai.request(server)
+      .post('/api/auth/login')
+      .send({ email: 'tunde@mail.com', password: 'wrong' })
+      .end((err, res) => {
+        expect(res.status).to.eql(404);
+        expect(res.body.status).to.eql('failed');
+        expect(res.body.error).to.eql('email or password is incorrect');
+        done();
+      });
+  });
+  it('should return success when user is logged in successfully', (done) => {
+    chai.request(server)
+      .post('/api/auth/login')
+      .send({ email: 'tunde@mail.com', password: 'secret' })
+      .end((err, res) => {
+        expect(res.status).to.eql(200);
+        expect(res.body.status).to.eql('success');
+        expect(res.body.data).have.property('id');
+        expect(res.body.data).have.property('firstName');
+        done();
+      });
+  });
+});
